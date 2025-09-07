9ff7333698868c2350c8bda522693dc05ca11150
Nick Cooke <nj.cooke@outlook.com>
2025-09-07 23:10:30 +0100

    fixing cross compilation - still

--- flake.nix
+++ flake.nix
@@ -7,87 +7,86 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    # Include both native dev and the Pi targets
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "armv7l-linux" ] (system:
+    # Host(s) you actually develop on:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
         lib  = pkgs.lib;
 
-        # Cross sets for when host != target
+        # Cross targets (Pi)
         pkgsAarch64 = pkgs.pkgsCross.aarch64-multiplatform;
         pkgsArmv7l  = pkgs.pkgsCross.armv7l-hf-multiplatform;
 
         packageName = "kvmSwitch";
 
-        # Prefer libgpiod v1. Some nixpkgs have libgpiod1, some libgpiod_1.
-        gpiodV1 = (pkgs.libgpiod1 or pkgs.libgpiod_1);
+        jailbreakUnbreak = pkgsU: pkg:
+          let hl = pkgsU.haskell.lib; in hl.markUnbroken (hl.doJailbreak pkg);
 
-        # Helper to instantiate from the generated nix file
-        mkFromGenerated = pkgsU:
+        withOverrides = pkgsU:
+          pkgsU.haskellPackages.override {
+            overrides = self': super: { };
+          };
+
+        
+        mkKvmWith = pkgsU:
           let
-            # the generated file from `cabal2nix . > nix/kvmSwitch.nix`
-            drv0 = pkgsU.callPackage ./nix/kvmSwitch.nix {
-              # force pkg-config to pick v1 of libgpiod
-              libgpiod = (pkgsU.libgpiod1 or pkgsU.libgpiod_1);
+            hp0  = pkgsU.haskellPackages;
+            gpiodV1 = (pkgsU.libgpiod1 or pkgsU.libgpiod_1);
+            drv0 = hp0.callCabal2nix packageName self {
+              libgpiod = gpiodV1;  # for pkg-config-depends: libgpiod
+              gpiod    = gpiodV1;  # for extra-libraries: gpiod
             };
 
-            # nudge Haskell lib if needed
-            drv1 = pkgsU.haskell.lib.markUnbroken
-                     (pkgsU.haskell.lib.doJailbreak drv0);
+            drv1 = pkgsU.haskell.lib.markUnbroken (pkgsU.haskell.lib.doJailbreak drv0);
 
-            pcPath = pkgs.lib.makeSearchPath "lib/pkgconfig" [
-              (pkgsU.libgpiod1 or pkgsU.libgpiod_1)
-            ];
+            pcPath = pkgs.lib.makeSearchPath "lib/pkgconfig" [ gpiodV1 ];
           in
           drv1.overrideAttrs (old: {
             nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.pkg-config ];
-            buildInputs       = (old.buildInputs       or []) ++ [ (pkgsU.libgpiod1 or pkgsU.libgpiod_1) ];
-            # Ensure *only* v1 is seen by pkg-config
+            buildInputs       = (old.buildInputs       or []) ++ [ gpiodV1 ];
             PKG_CONFIG_PATH   = pcPath;
             PKG_CONFIG_LIBDIR = pcPath;
+            strictDeps = true;
           });
 
-        # Per-target derivations
-        kvmNative =
-          mkFromGenerated pkgs;
 
-        kvmAarch64 =
-          if pkgs.stdenv.hostPlatform.system == "aarch64-linux"
-          then mkFromGenerated pkgs
-          else mkFromGenerated pkgsAarch64;
+        kvmNative  = mkKvmWith pkgs;
+        kvmAarch64 = mkKvmWith pkgsAarch64; # Pi 64-bit OS
+        kvmArmv7l  = mkKvmWith pkgsArmv7l;  # Pi 32-bit OS (common)
 
-        kvmArmv7l =
-          if pkgs.stdenv.hostPlatform.system == "armv7l-linux"
-          then mkFromGenerated pkgs
-          else mkFromGenerated pkgsArmv7l;
-      in
-      {
+        gpiodV1 = pkgs.libgpiod1 or pkgs.libgpiod_1;
+      in {
         packages.${packageName}           = kvmNative;
         packages.default                  = kvmNative;
         packages."${packageName}-aarch64" = kvmAarch64;
         packages."${packageName}-armv7l"  = kvmArmv7l;
 
+        # nix run .#kvmSwitch
+        apps =
+          let app = flake-utils.lib.mkApp { drv = kvmNative; };
+          in {
+            default   = app;
+            kvmSwitch = app;
+          };
+
         devShells.default = pkgs.mkShell {
           inputsFrom = lib.optional (kvmNative ? env) kvmNative.env;
-          buildInputs = [
-            pkgs.haskellPackages.haskell-language-server
-            pkgs.haskellPackages.hoogle
-            pkgs.cabal-install
-            pkgs.ghcid
-            pkgs.lsof
-            pkgs.xdotool
-            pkgs.pkg-config
-            pkgs.cachix
-          ] ++ lib.optionals pkgs.stdenv.isLinux [
-            (pkgs.libgpiod1 or pkgs.libgpiod_1)
+          buildInputs = with pkgs; [
+            haskellPackages.haskell-language-server
+            haskellPackages.hoogle
+            cabal-install
+            ghcid
+            lsof
+            xdotool
+            pkg-config
+            gpiodV1
+            cachix
           ];
-
-          shellHook = lib.optionalString pkgs.stdenv.isLinux ''
-            export LD_LIBRARY_PATH=${lib.makeLibraryPath [ (pkgs.libgpiod1 or pkgs.libgpiod_1) ]}:$LD_LIBRARY_PATH
+          shellHook = ''
+            export LD_LIBRARY_PATH=${lib.makeLibraryPath [ gpiodV1 ]}:$LD_LIBRARY_PATH
           '';
         };
 
-        # For older tools that expect devShell
         devShell = self.devShells.${system}.default;
       });
 }
